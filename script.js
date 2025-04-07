de2661b44ef40552fe08e303a476aa92ce4564f9
Daba DIALLO <diallodaba527@gmail.com>
2025-04-07 09:20:54 +0000

    premier commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const target = document.querySelector('.conte');
+  let isColored = false; // État actuel : faux = blanc, vrai = bleu foncé
+
+  document.getElementById("changeColor").addEventListener("click", function () {
+    if (isColored) {
+      target.style.backgroundColor = "#FFFFFF"; // Blanc
+    } else {
+      target.style.backgroundColor = "#130C67"; // Bleu foncé
+    }
+    isColored = !isColored; // On inverse l'état
+  });
+
+
