b5fc78cac84baa286ad1307f8c4803b360e294c6
Daba DIALLO <diallodaba527@gmail.com>
2025-04-08 00:16:34 +0000

    A4 ok

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Bouton pour changer la couleur de la page
 const target = document.querySelector('.conte');
   let isColored = false; // État actuel : faux = blanc, vrai = bleu foncé
 
@@ -9,17 +10,25 @@ const target = document.querySelector('.conte');
     }
     isColored = !isColored; // On inverse l'état
   });
-//Téléchargement
 
+//Téléchargement
 function download() {
-  const element = document.getElementById('download'); // Assurez-vous que l'ID correspond à celui de l'élément que vous souhaitez convertir
-  const options = {
-    margin: 1,
+  const element = document.getElementById('download');
+  const opt = {
+    margin: 0,
     filename: 'CV_Daba_DIALLO.pdf',
-    image: { type: 'jpeg', quality: 0.98 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+    image: { type: 'png', quality: 1 },
+    html2canvas: {
+      scale: 2,
+      useCORS: true,
+      scrollY: 0,
+    },
+    jsPDF: {
+      unit: 'mm',
+      format: 'a4',
+      orientation: 'portrait',
+    }
   };
 
-  html2pdf().set(options).from(element).save();
+  html2pdf().set(opt).from(element).save();
 }
